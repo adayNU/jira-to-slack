36261ea91b4f96b61b0e3e351e48da4fe30483d0
aday <aday@liveramp.com>
2017-03-21 17:52:34 -0700

    v0.0.3

---
+++ lib/jira-to-slack.rb
@@ -0,0 +1,23 @@
+class JiraToSlack
+  def self.convert(string)
+    #striketrhough
+    string.gsub!(/(^| )-(\b.*?)-( |$)/) {|e| "#{$1}~#{$2}~#{$3}"}
+    #bullets
+    string.gsub!(/(^|\n)(\*+?)( [^\*]*?)(\n|$)/m) {|e| "#{$1}#{"\t" * ($2 ? ($2.length - 1) : 0)}\u2022#{$3}#{$4}"}
+    #numbered lists
+    number_count = 0
+    string.gsub!(/^# /) {|e| number_count += 1; "#{number_count}. "}
+    #inline code
+    string.gsub!(/\{\{(.*?)\}\}/) {|e| "`#{$1}`"}
+    #multiline code
+    string.gsub!(/\{code\}/,"```")
+    #link
+    string.gsub!(/\[([^~|]*?)\]/) {|e| "#{$1}"}
+    #link with alias
+    string.gsub!(/\[(.*?)\|(.*?)\]/) {|e| "<#{$2}|#{$1}>"}
+    #users
+    string.gsub!(/\[\~(.*?)\]/) {|e| "<@#{$1}>"}
+
+    string
+  end
+end
