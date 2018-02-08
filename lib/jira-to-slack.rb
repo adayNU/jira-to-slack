5c2a3fd7748ff58b55c4e083ef2a2104e8ab88e1
Andy Day <aday@liveramp.com>
2018-02-08 10:01:39 -0800

    added support for multiline quotes and headings

--- lib/jira-to-slack.rb
+++ lib/jira-to-slack.rb
@@ -1,22 +1,28 @@
 class JiraToSlack
   def self.convert(string)
-    #striketrhough
+    # striketrhough
     string.gsub!(/(^| )-(\b.*?)-( |$)/) {|e| "#{$1}~#{$2}~#{$3}"}
-    #bullets
+    # bullets
     string.gsub!(/(^|\n)(\*+?)( [^\*]*?)(\n|$)/m) {|e| "#{$1}#{"\t" * ($2 ? ($2.length - 1) : 0)}\u2022#{$3}#{$4}"}
-    #numbered lists
+    # numbered lists
     number_count = 0
     string.gsub!(/^# /) {|e| number_count += 1; "#{number_count}. "}
-    #inline code
+    # inline code
     string.gsub!(/\{\{(.*?)\}\}/) {|e| "`#{$1}`"}
-    #multiline code
+    # multiline code
     string.gsub!(/\{code\}/,"```")
-    #link
+    # link
     string.gsub!(/\[([^~|]*?)\]/) {|e| "#{$1}"}
-    #link with alias
+    # link with alias
     string.gsub!(/\[(.*?)\|(.*?)\]/) {|e| "<#{$2}|#{$1}>"}
-    #users
+    # users
     string.gsub!(/\[\~(.*?)\]/) {|e| "<@#{$1}>"}
+    # blockquote
+    string.gsub!(/^bq\./, ">")
+    # multiline quote
+    string.gsub!(/\{quote\}(.*?)\{quote\}/m) {|e| "#{$1.split("\n").map{|f| "> #{f}"}.join("\n")}"}
+    # headings
+    string.gsub!(/^h[0-9]\. (.*$)/) {|e| "*#{$1}*"}
 
     string
   end
